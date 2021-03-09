75c6cdd5db21d5cacf553c8f4446cbe689b21486
marius0022 <mgrigoriu12@gmail.com>
2021-03-09 11:51:06 +0200

    new

--- mustetandaniel.com/script.js
+++ mustetandaniel.com/script.js
@@ -1,7 +1,14 @@
-/*function validateForm() {
-    var x = document.forms["myForm"]["fname"].value;
-    if (x == "" || x == null) {
-        alert("Name must be filled out");
+
+function validateForm() {
+    var x = document.forms.form.name.form.email.value;
+    if (x == "" || x == null || x == " ") {
+        alert("Please Tell Me Your Name And Your Email Before Submiting");
         return false;
     }
-}*/
+    else {
+        var message = document.getElementById("name").value;
+        alert("Thank You" + " " + message);
+        return true;
+    }
+
+}
