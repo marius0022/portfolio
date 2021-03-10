6a1241783e49fdf8780488c904cb256f16d77130
marius0022 <mgrigoriu12@gmail.com>
2021-03-10 09:53:26 +0200

    way

--- mustetandaniel.com/script.js
+++ mustetandaniel.com/script.js
@@ -1,13 +1,21 @@
 
 function validateForm() {
-    var x = document.forms.form.name.form.email.value;
+    var x = document.forms.form.name.value;
+    var y = document.forms.form.email.value;
     if (x == "" || x == null || x == " ") {
-        alert("Please Tell Me Your Name And Your Email Before Submiting");
+
+        document.getElementById("error").innerHTML = "Please Tell Me Your Name So It's Easier For Me To Contact You Back!"
+        return false;
+    }
+    else if (y == "" || y == null || y == " ") {
+
+        document.getElementById("error1").innerHTML = "Please Tell Me Your E-Mail So It's Easier For Me To Contact You Back!"
         return false;
     }
     else {
-        var message = document.getElementById("name").value;
-        alert("Thank You" + " " + message);
+        var uname = document.getElementById("name").value;
+        alert("Thank You" + " " + uname.charAt(0).toUpperCase() + uname.slice(1));
+
         return true;
     }
 
