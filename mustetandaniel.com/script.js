f5b5e92a891eb1c95dc061279131fb1ececc5160
marius0022 <mgrigoriu12@gmail.com>
2021-03-18 10:48:25 +0200

    new

--- mustetandaniel.com/script.js
+++ mustetandaniel.com/script.js
@@ -1,35 +1,14 @@
-const header = document.querySelector("#imgblur");
-const sectionOne = document.querySelector(".imagefull");
-const navbar = document.querySelector(".header");
-const button = document.querySelector("#myBtn");
-const sectionOneOptions = {
-    threshold: 0.1
-};
 
-const sectionOneObserver = new IntersectionObserver(function (
-    entries,
-    sectionOneObserver
-) {
-    entries.forEach(entry => {
+window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+    header.classList.toggle('header-scrolled', window.scrollY > 0);
 
-        if (!entry.isIntersecting) {
-            header.classList.add("nav-scrolled");
-            navbar.classList.add("header-scrolled");
-            button.classList.add("myBtn");
-        }
-      
-        else {
-            header.classList.remove("nav-scrolled");
-            navbar.classList.remove("header-scrolled");
-            button.classList.remove("myBtn");
+});
+const nav = document.querySelector('nav');
 
-        }
-
-    });
-},
-sectionOneOptions);
-sectionOneObserver.observe(sectionOne);
-
+ document.querySelector('.toggler').onclick = function () {
+     this.classList.toggle('active');
+     nav.classList.toggle('active');}
 
 
 
